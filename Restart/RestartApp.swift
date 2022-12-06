b54e203c43c196c1ab524f98688a35b066177a23
Raul Astete <rastete@acculynx.com>
2022-12-06 09:49:07 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Raul Emilio Astete Acuna on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
